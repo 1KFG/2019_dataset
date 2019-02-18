0a2cd951f864425eaa7d2535b7fc83bab295db07
Jason Stajich <jasonstajich.phd@gmail.com>
2019-02-18 14:05:40 -0800

    trying to run with parallel in R

--- jobs/run_stats_R.sh
+++ jobs/run_stats_R.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
-#SBATCH -p stajichlab --nodes 1 --ntasks 2 --mem 32G --out logs/summarize_genomes.%A.log
+#SBATCH -p stajichlab --nodes 1 --ntasks 64 --mem 32G --out logs/summarize_genomes.%A.log
 
 Rscript Rscripts/summarize_genomes.R
