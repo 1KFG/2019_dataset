5fb406c28912f6e613a2f7e4e0d8c077f0465792
Jason Stajich <jason.stajich@gmail.com>
2019-04-29 00:03:41 -0700

    some returns

--- Rscripts/plot_genome_stats.R
+++ Rscripts/plot_genome_stats.R
@@ -22,7 +22,15 @@ p <- ggplot(sumstats, aes(x=intronlen_mean,y=exonlen_mean)) + geom_point() +
     scale_color_brewer(palette="Set1") + theme(legend.position="none")
 print(p)
 
-p <- ggplot(sumstats, aes(x=genome_size,y=genect)) + geom_point() + 
-  theme_minimal() + ggtitle("Genome size vs Gene Count") +
+p <- ggplot(sumstats, aes(x=genelen_mean,y=exonlen_mean)) + geom_point() + 
+  theme_minimal() + ggtitle("Intron size vs exon size genes (mean)") +
     scale_color_brewer(palette="Set1") + theme(legend.position="none")
+print(p)
+
+pdf(sprintf("%s/%s","plots","genome_sizes.pdf"),onefile=TRUE)
+
+p <- ggplot(sumstats, aes(x=genome_size / 1000000 ,y=genect)) + geom_point() + 
+  theme_minimal() + ggtitle("Fungal Genome size vs Gene Count") +
+  		  xlab("Genome size (Mb)") + ylab("Gene count") +
+    scale_color_brewer(palette="Set1") + theme(legend.position="none") + 
 print(p)
