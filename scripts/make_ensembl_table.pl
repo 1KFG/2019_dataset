432c39ff70e72d9e050af75a6483a18aa9405e7e
Jason Stajich <jasonstajich.phd@gmail.com>
2021-01-21 20:37:32 -0800

    use sqlite taxonomy db

--- scripts/make_ensembl_table.pl
+++ scripts/make_ensembl_table.pl
@@ -1,17 +1,82 @@
 #!/usr/bin/env perl
-
+use strict;
+use warnings;
 use Bio::DB::Taxonomy;
+use DB_File;
+
+my %lookup;
+my $cachefile = 'names.idx';
+
+tie %lookup, "DB_File", $cachefile, O_RDWR | O_CREAT, 0666, $DB_HASH
+  or die "Cannot open file '$cachefile': $!\n";
 
 my $namesfile = 'tmp/taxa/names.dmp';
 my $nodesfile = 'tmp/taxa/nodes.dmp';
-my $indexdir  = 'tmp/indexes';
-if ( ! -d $indexdir ) {
-    mkdir($indexdir);
-}
-my $db = Bio::DB::Taxonomy->new(-source      => 'flatfile', 
-				-directory   => $indexdir,
-				-nodesfile   => $nodesfile, 
-				-verbose     => 1,
-				-namesfile   => $namesfile);
 
-print ($db),"\n";
+my $db = Bio::DB::Taxonomy->new(
+        -source    => 'sqlite',
+        -nodesfile => $nodesfile,
+        -namesfile => $namesfile,
+);
+
+my $dir = shift      || 'source/Ensembl/pep';
+opendir( DIR, $dir ) || die $!;
+print join( ",", qw(NAME GENUS SPECIES STRAIN TAXONOMY)),"\n";
+
+foreach my $file ( readdir(DIR) ) {
+    next if $file =~ /^\./;
+    my ( $genus, $species, $strain, $spname );
+
+    if ( $file =~ /^(\S+)_gca_/ || $file =~ /^(\S+)\.GCA_/ ) {
+        $spname = $1;
+    } else {
+        ($spname) = split( /\./, $file );
+    }
+    ( $genus, $species, $strain ) = split( /_/, $spname, 3 );
+    my $species_string = join( " ", $genus, $species );
+    warn("species string is $species_string\n");
+    my $str;
+    if ( exists $lookup{$species_string} ) {
+        $str = $lookup{$species_string};
+    } else {
+        my $h = $db->get_taxonid($species_string);
+        #warn("taxonid is $h\n");
+
+        if ( !$h ) {
+            $h = $db->get_taxonid($genus);
+        }
+        my $node = $db->get_taxon( -taxonid => $h );
+
+        if ($node) {
+            my @tax;
+            my %ranks;
+            while ($node) {
+
+                #	    print("rank=",$node->rank, ". node name is ",
+                #		  join(",",@{$node->name('scientific')},"\n"));
+
+                if ( $node->rank ne 'no rank' ) {
+                    $ranks{ $node->rank } = scalar @tax;
+                }
+
+                push @tax, [ $node->rank, @{ $node->name('scientific') } ];
+
+                my $ancestor = $node->ancestor;
+                $node = $ancestor;
+            }
+            my $str = join(
+                ";",
+                map {
+                    exists $ranks{$_}
+                      ? join( ":", @{ $tax[ $ranks{$_} ] } )
+                      : ''
+                } qw(phylum subphylum class subclass family genus)
+            );
+            $lookup{$species_string} = $str;
+        }
+        else {
+            $str = "";
+        }
+    }
+    print join( ",", $spname, $genus,$species,$strain || '', $str || ''), "\n";
+}
--- scripts/make_jgi_taxonomy_table.pl
+++ scripts/make_jgi_taxonomy_table.pl
@@ -7,27 +7,18 @@ use DB_File;
 my %lookup;
 my $cachefile = 'names.idx';
 
-tie %lookup, "DB_File", $cachefile, O_RDWR|O_CREAT, 0666, $DB_HASH 
+tie %lookup, "DB_File", $cachefile, O_RDWR|O_CREAT, 0666, $DB_HASH
     or die "Cannot open file '$cachefile': $!\n";
 
 my $namesfile = 'tmp/taxa/names.dmp';
 my $nodesfile = 'tmp/taxa/nodes.dmp';
-my $indexdir  = 'indexes';
 
-my $db;
-if ( ! -d $indexdir ) {
-    mkdir($indexdir);
-}
-if ( -f "$indexdir/nodes") {
-    $db = Bio::DB::Taxonomy->new(-source      => 'flatfile', 
-				 -directory   => $indexdir);
-    
-} else {
-    $db = Bio::DB::Taxonomy->new(-source      => 'flatfile', 
-				-directory   => $indexdir,
-				-nodesfile   => $nodesfile, 
-				-namesfile   => $namesfile);
-}
+
+my $db = Bio::DB::Taxonomy->new(
+        -source    => 'sqlite',
+        -nodesfile => $nodesfile,
+        -namesfile => $namesfile,
+);
 my $header = <>;
 print $header;
 while(<>) {
@@ -42,25 +33,25 @@ while(<>) {
 #	print join(";",($species,$clade)),"\n";
 	my $h = $db->get_taxonid($species_string);
 #	print("taxonid is $h\n");
-	
+
 	if ( ! $h ) {
 	    $h = $db->get_taxonid($genus);
 	}
 	my $node = $db->get_taxon(-taxonid => $h);
-	
+
 	if ( $node ) {
 	    my @tax;
 	    my %ranks;
-	    while ( $node ) {	    
+	    while ( $node ) {
 		#	    print("rank=",$node->rank, ". node name is ",
 		#		  join(",",@{$node->name('scientific')},"\n"));
-		
+
 		if ( $node->rank ne 'no rank' ) {
 		    $ranks{$node->rank} = scalar @tax;
 		}
-		
+
 		push @tax, [ $node->rank, @{$node->name('scientific')} ];
-		
+
 		my $ancestor = $node->ancestor;
 		$node = $ancestor;
 	    }
@@ -69,9 +60,8 @@ while(<>) {
 			   qw(phylum subphylum class subclass family genus));
 	    $lookup{$species_string} = $str;
 	} else {
-	    $str = "";	    
+	    $str = "";
 	}
     }
     print join(",", $name,$species,$strain,$str,@rest),"\n";
 }
-
